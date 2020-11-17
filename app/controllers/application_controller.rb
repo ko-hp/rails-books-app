89acd7065589bb94b88b1b170556d9eae6fd345a
ko-hp <kohpgg@gmail.com>
2020-11-17 17:22:50 +0900

    ログインしないと使用できないように

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 class ApplicationController < ActionController::Base
   around_action :switch_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
